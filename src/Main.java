989c3e038d4b2337a3fccdea6fcacf8e060fc5b8
Hyann <contato.hyann@gmail.com>
2026-01-25 02:15:17 -0300

    Aperfeicoando o projeto

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String nome, cargo;
+        double salarioBase, novoSalario;
+
+        final double ESTAGIARIO = 0.05;
+        final double JUNIOR = 0.10;
+        final double PLENO = 0.15;
+
+        System.out.println("Bem-vindo ao CadastraFacil!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        nome = scanner.nextLine();
+
+        System.out.print("Agora seu cargo: ");
+        cargo = scanner.nextLine().toLowerCase(Locale.ROOT);
+
+        System.out.print("Por fim, digite seu salário: ");
+        salarioBase = scanner.nextDouble();
+
+        System.out.println("\n===== RELATÓRIO =====");
+        System.out.println("Funcionário: " + nome);
+        System.out.println("Cargo: " + cargo);
+        System.out.printf("Salário base: R$ %.0f%n", salarioBase);
+
+        if (cargo.equals("estagiario")) {
+            novoSalario = salarioBase + (salarioBase * ESTAGIARIO);
+            System.out.println("Aumento: 5%");
+        } else if (cargo.equals("junior")) {
+            novoSalario = salarioBase + (salarioBase * JUNIOR);
+            System.out.println("Aumento: 10%");
+        } else if (cargo.equals("pleno")) {
+            novoSalario = salarioBase + (salarioBase * PLENO);
+            System.out.println("Aumento: 15%");
+        } else {
+            System.out.println("Cargo inválido!");
+            scanner.close();
+            return;
         }
+
+        System.out.printf("Salário final: R$ %.0f%n", novoSalario);
+        System.out.println("=====================");
+
+        scanner.close();
     }
 }
